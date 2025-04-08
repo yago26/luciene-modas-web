4bffcc3790b42998047d7403414846e0ec45a9ab
yago jordas <yagojordasdonp@gmail.com>
2025-04-08 11:29:52 -0300

    DAW II - Aula 01

---
+++ src/app/aluno/page.js
@@ -0,0 +1,11 @@
+export default function Aluno() {
+  return (
+    <>
+      <h1>Vamos comer</h1>
+    </>
+  );
+}
+
+// Rotas
+// Componentes
+// Exportar como função apenas se o componente for único
