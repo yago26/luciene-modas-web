dd5b4959923b55bd461e1f23cc21555127d7c373
yago jordas <yagojordasdonp@gmail.com>
2025-09-16 09:25:17 -0300

    Correção da adição de produtos com valres com vírgulas

--- src/app/api/produtos/route.js
+++ src/app/api/produtos/route.js
@@ -19,27 +19,45 @@ export async function POST(req) {
   try {
     const { nome, sobre, valor, categoria, imagemUrl, estoque } =
       await req.json();
+
+    const valorFormatado = String(valor).replace(",", ".");
+
     const id = uuidv4();
 
     if (!sobre && !estoque) {
       db.query(
         "INSERT INTO tb_produtos (id, nome, valor, categoria, imagem) VALUES ($1, $2, $3, $4, $5)",
-        [id, nome, Number(valor), categoria, imagemUrl]
+        [id, nome, Number(valorFormatado), categoria, imagemUrl]
       );
     } else if (!sobre) {
       db.query(
         "INSERT INTO tb_produtos (id, nome, valor, categoria, imagem, estoque) VALUES ($1, $2, $3, $4, $5, $6)",
-        [id, nome, Number(valor), categoria, imagemUrl, Number(estoque)]
+        [
+          id,
+          nome,
+          Number(valorFormatado),
+          categoria,
+          imagemUrl,
+          Number(estoque),
+        ]
       );
     } else if (!estoque) {
       db.query(
         "INSERT INTO tb_produtos (id, nome, sobre, valor, categoria, imagem) VALUES ($1, $2, $3, $4, $5, $6)",
-        [id, nome, sobre, Number(valor), categoria, imagemUrl]
+        [id, nome, sobre, Number(valorFormatado), categoria, imagemUrl]
       );
     } else {
       db.query(
         "INSERT INTO tb_produtos (id, nome, sobre, valor, categoria, imagem, estoque) VALUES ($1, $2, $3, $4, $5, $6, $7)",
-        [id, nome, sobre, Number(valor), categoria, imagemUrl, Number(estoque)]
+        [
+          id,
+          nome,
+          sobre,
+          Number(valorFormatado),
+          categoria,
+          imagemUrl,
+          Number(estoque),
+        ]
       );
     }
 
